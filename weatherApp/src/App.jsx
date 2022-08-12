3de49eb030c74c1b18ad6a7ae8a380f5af689928
Sanjay Sharma <96072511+sanjaysharma94@users.noreply.github.com>
2022-08-12 23:25:08 +0530

    Update App.jsx

--- weatherApp/src/App.jsx
+++ weatherApp/src/App.jsx
@@ -5,20 +5,44 @@ import { useEffect } from 'react'
 import axios from "axios"
 import { useDispatch , useSelector } from "react-redux"
 import { Slider } from './components/Corousel'
+import { useState } from 'react'
 
 
 function App() {
 
-  const city = useSelector((store)=>store.credential.city)
+  const {city} = useSelector((store)=>store.credential)
+  //const {latitude,longitude}= useSelector((store)=>store.credential)
   
-  if(!city==""){
-    console.log("hii")
+ 
+  
+//console.log(latitude,longitude)
+let Id;
+
   useEffect(()=>{
-     axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2d9c313e67589677085ed508b96ae174`)
-       .then((r)=>console.log(r.data))
-      .catch((e)=>console.log(e.message))
+
+    
+
+  
+     Id = setTimeout( () => {
+
+
+      axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2d9c313e67589677085ed508b96ae174`)
+        .then((r)=>console.log(r.data))
+       .catch((e)=>console.log(e.message))
+     
+
+      }, 5000); return () => clearTimeout(Id);
+
+    
+  
+    
+    // axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude={part}&appid=2d9c313e67589677085ed508b96ae174`)
+    //    .then((r)=>console.log(r.data))
+    //   .catch((e)=>console.log(e.message))
+
+    
   },[city])
-  }
+
  
    
 
