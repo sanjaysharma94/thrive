eb47a931c9d0ea2ea7305eff2e78d170dca4b463
Sanjay Sharma <96072511+sanjaysharma94@users.noreply.github.com>
2022-08-14 23:01:30 +0530

    Update action.js

--- weatherApp/src/Redux/action.js
+++ weatherApp/src/Redux/action.js
@@ -1,5 +1,6 @@
 export const UPDATE_CITY = "UPDATE_CITY"
 export const COORDINATES = "COORDINATES"
+export const FORECAST ="FORECAST"
 
 export const updateCity = (payload) =>{
     return {
@@ -11,6 +12,14 @@ export const updateCity = (payload) =>{
 export const coordinates = (payload) =>{
     return {
         type: COORDINATES,
+        payload,
+    }
+}
+
+
+export const forecast = (payload) =>{
+    return {
+        type: FORECAST,
         payload,
     }
 }
