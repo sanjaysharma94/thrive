78fb184b0f497e490b8c1e5f33570ff968baaf81
Sanjay Sharma <96072511+sanjaysharma94@users.noreply.github.com>
2022-08-15 22:15:34 +0530

    Update Corousel.jsx

--- weatherApp/src/components/Corousel.jsx
+++ weatherApp/src/components/Corousel.jsx
@@ -7,7 +7,7 @@ export const Slider = () => {
   const jankari = useSelector((store)=>store.credential.forecast) 
     
   if(jankari){ return <div>
-        <Carousel className='styles-module_item-provider__YgMwz' show={5} slide={2}swiping={true} > 
+        <Carousel className='styles-module_item-provider__YgMwz' show={7} slide={2}swiping={true} > 
 
 {jankari.map((users)=><div key={users.dt} className='scrollable-div' color="#2d66c3">{users?.temp?.day}°C🌐</div>)}
     
