bebc8665b9d7ce1f29d831b3959c57bab6a33ad1
Sanjay Sharma <96072511+sanjaysharma94@users.noreply.github.com>
2022-09-08 13:14:55 +0530

    adding luxon library

--- weatherApp/src/components/Corousel.jsx
+++ weatherApp/src/components/Corousel.jsx
@@ -1,7 +1,7 @@
-import { Carousel } from '@trendyol-js/react-carousel';
+
 import { useEffect } from 'react';
 import { useRef, useState } from 'react';
-
+import { DateTime } from "luxon";
 import {  useSelector } from "react-redux"
 
 export const Slider = () => {
@@ -9,7 +9,11 @@ export const Slider = () => {
 
 const  data = useSelector((store)=>store.credential.forecast)
          
+let time = (tm)=>{
 
+  return DateTime.fromSeconds(tm).toFormat("cccc")
+
+};
     
   return <div>
     <h1>hello</h1>
