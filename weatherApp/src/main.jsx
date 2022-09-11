1d8f8b6e57542b326fcaa675e72b3063f6d201be
Sanjay Sharma <96072511+sanjaysharma94@users.noreply.github.com>
2022-09-11 13:38:08 +0530

    Update main.jsx

--- weatherApp/src/main.jsx
+++ weatherApp/src/main.jsx
@@ -1,4 +1,4 @@
-import{ React }from 'react'
+import{ React , StrictMode}from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
@@ -6,10 +6,10 @@ import {store } from "../src/Redux/store"
 import { Provider } from "react-redux"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <StrictMode>
      <Provider store={store}>
     <App />
     </Provider>
-    </React.StrictMode>
+    </StrictMode>
   
 )
